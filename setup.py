720e7fea1ac2182f89a6f216d657e2bbcd3b4658
Vladimir Eremeev <wl2776@gmail.com>
2023-02-25 16:30:01 +0300

    Rename python scripts, add parameters

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+import os
+from glob import glob
 from setuptools import setup
 
 package_name = 'alphabot2_ros2'
@@ -10,6 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('launch/*launch.[pxy][yma]*'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -19,7 +22,6 @@ setup(
     license='BSD-3-Clause',
     tests_require=['pytest'],
     entry_points={
-        'console_scripts': ['alphabot2 = alphabot2_ros2.alphabot2_ros2_node:main',
-        ],
+        'console_scripts': ['alphabot2 = alphabot2_ros2.alphabot2_node:main',],
     },
 )
